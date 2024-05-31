4b0c22cf3bf6ff40c1cb9fd9726311d51a05b023
minh <rnrnck78@naver.com>
2024-05-31 14:57:06 +0900

    fix user entity 수정

--- src/main/java/io/elice/shoppingmall/user/model/User.java
+++ src/main/java/io/elice/shoppingmall/user/model/User.java
@@ -34,4 +34,17 @@ public class User {
 
 //    @OneToMany(mappedBy = "user")
 //    private List<Order> orders;
+
+    @Getter
+    public enum Role {
+        USER("ROLE_USER","일반 회원"), ADMIN("ROLE_ADMIN","관리자");
+
+        private final String key;
+        private final String title;
+
+        Role(String key, String title) {
+            this.key = key;
+            this.title = title;
+        }
+    }
 }
