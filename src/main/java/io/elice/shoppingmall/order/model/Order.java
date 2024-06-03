d5121d19076ed5afd8697e54375951822f8333b3
[Cloud3] 신준영 <2261-jy.shin@users.noreply.kdt-gitlab.elice.io>
2024-06-03 12:39:28 +0900

    Fix order by code review

--- src/main/java/io/elice/shoppingmall/order/model/Order.java
+++ src/main/java/io/elice/shoppingmall/order/model/Order.java
@@ -4,7 +4,9 @@ import io.elice.shoppingmall.audit.BaseEntity;
 import io.elice.shoppingmall.user.model.User;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,19 +15,26 @@ import java.util.List;
 @Table(name = "orders")
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 public class Order extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long orderId;
 
-//    @Enumerated(EnumType.STRING)
-//    private OrderStatus orderStatus;
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
 
-
+    @Column(nullable = false)
     private int orderTotalPrice;
+
+    @Column(length = 50)
     private String orderRequest;
+
+    @Column(nullable = false, length = 200)
     private String orderSummaryTitle;
+
     private boolean isDeleted;
 
     @OneToMany(mappedBy = "order")
--- src/main/java/io/elice/shoppingmall/order/model/OrderItem.java
+++ src/main/java/io/elice/shoppingmall/order/model/OrderItem.java
@@ -1,34 +1,41 @@
 package io.elice.shoppingmall.order.model;
 
 import io.elice.shoppingmall.audit.BaseEntity;
+import io.elice.shoppingmall.book.model.Book;
 import io.elice.shoppingmall.user.model.User;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 public class OrderItem extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long orderItemId;
 
+    @Column(nullable = false)
     private int orderItemPrice;
+
+    @Column(nullable = false)
     private int orderItemTotalPrice;
+
+    @Column(nullable = false)
     private int orderItemQuantity;
+
     private boolean isDeleted;
 
     @ManyToOne
     @JoinColumn(name = "order_id")
     private Order order;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User user;
-
-//    @OneToOne
-//    @JoinColumn(name = "book_id")
-//    private Book book;
+    @OneToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
 }
