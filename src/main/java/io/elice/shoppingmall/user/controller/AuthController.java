fcda5ad0c6b1e9b6a0e5a6b0435da164cbd3acc7
[Cloud3] 신준영 <2261-jy.shin@users.noreply.kdt-gitlab.elice.io>
2024-06-07 10:28:59 +0000

    Revert "Revert "Merge branch 'dev' into 'feature-FE-order-admin'""
    This reverts commit 4785bd9022a25a8d205c6118272be73adfbf0b4f

--- src/main/java/io/elice/shoppingmall/user/controller/AuthController.java
+++ src/main/java/io/elice/shoppingmall/user/controller/AuthController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,13 +21,17 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@Valid @RequestBody LoginDto loginDto) {
+    public ResponseEntity<?> login(@RequestBody LoginDto loginDto) {
         //Spring Security의 filter chain에서 자동으로 처리
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PostMapping("/join")
-    public ResponseEntity<?> join(@RequestBody JoinDto joinDto) {
+    public ResponseEntity<?> join(@Valid @RequestBody JoinDto joinDto, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            log.error(bindingResult.getFieldError().getDefaultMessage());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
         return new ResponseEntity<>(authService.join(joinDto), HttpStatus.CREATED);
     }
 }
