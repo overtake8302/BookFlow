4785bd9022a25a8d205c6118272be73adfbf0b4f
[Cloud3] 신준영 <2261-jy.shin@users.noreply.kdt-gitlab.elice.io>
2024-06-07 10:10:10 +0000

    Revert "Merge branch 'dev' into 'feature-FE-order-admin'"
    This reverts commit 46edc859a667ab14b475d85aa7173094c2856fe2

--- src/main/java/io/elice/shoppingmall/category/controller/CategoryController.java
+++ src/main/java/io/elice/shoppingmall/category/controller/CategoryController.java
@@ -5,28 +5,42 @@ import io.elice.shoppingmall.category.service.CategoryService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @CrossOrigin
 @RestController
-@RequestMapping("/api/admin/categories")
-public class AdminCategoryController {
+@RequestMapping("/api")
+public class CategoryController {
 
     private final CategoryService categoryService;
 
-    public AdminCategoryController(CategoryService categoryService) {
+    public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
     }
 
-    @PostMapping
+    // 일반 사용자용 API
+    @GetMapping("/categories")
+    public List<Category> getAllCategories() {
+        return categoryService.getAllCategories();
+    }
+
+    @GetMapping("/categories/{categoryId}/books")
+    public ResponseEntity<Category> getCategoryById(@PathVariable Integer categoryId) {
+        return ResponseEntity.ok(categoryService.getCategoryById(categoryId));
+    }
+
+    // 관리자용 API
+    @PostMapping("/admin/categories")
     public ResponseEntity<Category> createCategory(@RequestBody Category category) {
         return ResponseEntity.ok(categoryService.createCategory(category));
     }
 
-    @PutMapping("/{categoryId}")
+    @PutMapping("/admin/categories/{categoryId}")
     public ResponseEntity<Category> updateCategory(@PathVariable Integer categoryId, @RequestBody Category category) {
         return ResponseEntity.ok(categoryService.updateCategory(categoryId, category));
     }
 
-    @DeleteMapping("/{categoryId}")
+    @DeleteMapping("/admin/categories/{categoryId}")
     public ResponseEntity<Void> deleteCategory(@PathVariable Integer categoryId) {
         categoryService.deleteCategory(categoryId);
         return ResponseEntity.noContent().build();
