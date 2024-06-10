dd5346bf70a08fc714ed7b1c9a2e2c8065e9440f
leekyeongsu <javaleeks3@gmail.com>
2024-06-10 17:13:39 +0900

    Stop tracking files in src/main/generated

--- src/main/java/io/elice/shoppingmall/order/controller/UserOrderController.java
+++ src/main/java/io/elice/shoppingmall/order/controller/UserOrderController.java
@@ -1,8 +1,13 @@
 package io.elice.shoppingmall.order.controller;
 
 
-import io.elice.shoppingmall.order.model.*;
-import io.elice.shoppingmall.order.model.dto.*;
+import io.elice.shoppingmall.order.model.Order;
+import io.elice.shoppingmall.order.model.OrderDelivery;
+import io.elice.shoppingmall.order.model.OrderItem;
+import io.elice.shoppingmall.order.model.OrderMapper;
+import io.elice.shoppingmall.order.model.dto.OrderCreateDto;
+import io.elice.shoppingmall.order.model.dto.OrderResponseCombinedDto;
+import io.elice.shoppingmall.order.model.dto.OrdersPageDto;
 import io.elice.shoppingmall.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -11,7 +16,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -19,7 +23,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 
-@Controller
+@RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
 public class UserOrderController {
@@ -83,7 +87,11 @@ public class UserOrderController {
     }
 
     @PutMapping("/order/{orderId}")
-    public ResponseEntity<OrderResponseCombinedDto> putOrder(@PathVariable Long orderId, @RequestBody OrderCreateDto dto) {
+    public ResponseEntity<?> putOrder(@PathVariable Long orderId, @RequestBody @Validated OrderDeliveryEditDto dto,BindingResult error) {
+
+        if (error.hasErrors()) {
+            return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+        }
 
         Order editedOrder = orderService.editOrder(orderId, dto);
         OrderResponseCombinedDto  orderResponseDto = orderMapper.orderToOrderResponseCombinedDto(editedOrder);
