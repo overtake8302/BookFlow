432b0fe5124d8be3746427754ee3cccdc232aaa5
minh <rnrnck78@naver.com>
2024-06-10 16:16:37 +0900

    feat : 로그인, 회원가입 기능

---
+++ src/main/java/io/elice/shoppingmall/user/model/dto/ErrorResponseDto.java
@@ -0,0 +1,15 @@
+package io.elice.shoppingmall.user.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponseDto {
+    private int status;
+    private String message;
+}
---
+++ src/views/src/routes/user/auth/Login.js
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+
+const Login = () => {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const history = useHistory()
+
+
+  const LoginProcess = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:8080/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          const access = response.headers.get('access')
+          localStorage.setItem('access', access)
+          alert('로그인 성공')
+          history.push('/')
+          return
+        }
+        return response.json()
+      })
+      .then((json)=>{
+        //console.log(json);
+        if (json.status === 400 || json.status === 401) {
+          alert(json.message)
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
+  return (
+    <div>
+      <form>
+        <input 
+          type='text' 
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input 
+          type='text' 
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onClick={LoginProcess}>로그인</button>
+        <Link to="/join">회원가입</Link>
+      </form>
+    </div>
+  )
+}
+
+export default Login;
