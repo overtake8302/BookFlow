4b2ab473d437403f8577d2e3ef4034e8e49ce262
chohyeyeon <heyyeon77@naver.com>
2024-06-12 14:47:09 +0900

    Feat: 가격 정보 컴포넌트 데이터 연결

--- src/views/src/components/cart/CartPriceInfo.js
+++ src/views/src/components/cart/CartPriceInfo.js
@@ -1,30 +1,46 @@
 import "./CartPriceInfo.css"
 import {Link} from "react-router-dom";
 
-function CartPriceInfo(userName){
+function CartPriceInfo({cart}){
+    const unCheckedCart = cart.filter((book) => !book.checked);
 
+    let totalBookPrice = 0;
+    let checkedQuantity = 0;
+    unCheckedCart.forEach((book) => {
+        const bookPrice = book.book_price;
+        const bookQuantity = book.book_quantity;
+        totalBookPrice += bookPrice * bookQuantity;
+        checkedQuantity += bookQuantity;
+    });
+
+    const deliveryPrice = (totalBookPrice >= 50000)? 0 : 3000;
+    const totalPrice = totalBookPrice + deliveryPrice;
 
     return (
-        <div className="price-info">
-            <h3>결제정보</h3>
-            <div className="each-price">
-                <div id="book-price">
-                    상품금액: 50,000원
+        <div>
+            {unCheckedCart.map((book) => (
+                <div key={book.book_id} className="price-info">
+                    <h3>결제정보</h3>
+                    <div className="each-price">
+                        <div id="book-price">
+                            상품금액: {totalBookPrice}원
+                        </div>
+                        <div id="delivery-price">
+                            배송비: + {deliveryPrice}원
+                        </div>
+                        <hr/>
+                        <div id="total-price">
+                            총 결제금액: {totalPrice}원
+                        </div>
+                    </div>
+                    <div className="each-button">
+                        <button id="go-to-buy">구매하기 ({checkedQuantity})</button>
+                        <Link to="/">
+                            <button id="more-book">더 담으러 가기</button>
+                        </Link>
+                    </div>
                 </div>
-                <div id="delivery-price">
-                    배송비: + 3,000원
-                </div>
-                <hr/>
-                <div id="total-price">
-                    총 결제금액: 53,000원
-                </div>
-            </div>
-            <div className="each-button">
-                <button id="go-to-buy">구매하기 (2)</button>
-                <Link to="/">
-                    <button id="more-book">더 담으러 가기</button>
-                </Link>
-            </div>
+            ))}
         </div>
     );
 }
