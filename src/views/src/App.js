00e5af5a2968b6d186fae1c214acf2a34fde4c30
chohyeyeon <heyyeon77@naver.com>
2024-06-14 13:48:58 +0900

    Remove: 관리자 책 추가, 수정 관련 코드, 파일 삭제

--- src/views/src/App.js
+++ src/views/src/App.js
@@ -16,9 +16,6 @@ import {Order, OrderCompleted, OrderDetails, OrderDetailsByAdmin, OrderList, Ord
 
 import BookDetailTest from "./routes/bookTest/BookDetailTest";
 import Cart from "./routes/cart/Cart";
-<<<<<<< HEAD
-import BookAdd from "./routes/book/BookAdd";
-import BookUpdateDelete from "./routes/book/BookUpdateDelete";
 import BookDetail from "./routes/book/BookDetail";
 import BookDetailPage from "./routes/book/BookDetailPage";
 import BookAdminPage from "./routes/book/BookAdminPage";
@@ -79,12 +76,6 @@ function App() {
 
       <Route path="/bookDetail/:bookId">
         <BookDetail />
-      </Route>
-      <Route path="/bookAdd">
-        <BookAdd />
-      </Route>
-      <Route path="/bookUpdateDelete">
-        <BookUpdateDelete />
       </Route>
 
       <Route path="/admin/book/edit/:bookId">
