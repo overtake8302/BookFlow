739681e33e02f8cce55c96cb018760c42b834899
[Cloud3] 신준영 <2261-jy.shin@users.noreply.kdt-gitlab.elice.io>
2024-06-17 20:40:45 +0900

    Fix 이미지 업로드시 RequestPart 사용하도록 수정

--- src/main/java/io/elice/shoppingmall/book/controller/BookController.java
+++ src/main/java/io/elice/shoppingmall/book/controller/BookController.java
@@ -94,7 +94,7 @@ public class BookController {
 
     //책 수정
     @PutMapping("/admin/book/{bookId}")
-    public ResponseEntity<?> putBook(@PathVariable Long bookId,@ModelAttribute BookFormDto bookFormDto, @RequestParam(value = "images", required = false) List<MultipartFile> images) throws IOException {
+    public ResponseEntity<?> putBook(@PathVariable Long bookId,@ModelAttribute BookFormDto bookFormDto, @RequestPart(value = "images", required = false) List<MultipartFile> images) throws IOException {
 
         Book oldBook = bookService.getbookDetail(bookId);
         List<BookImg> bookImgs = new ArrayList<>();
