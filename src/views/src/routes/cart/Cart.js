4a162c5b7fe0921de9d6e973c9e8a78c41bd9a96
chohyeyeon <heyyeon77@naver.com>
2024-06-17 21:55:18 +0900

    Design: 장바구니 디자인 수정

--- src/views/src/routes/cart/Cart.js
+++ src/views/src/routes/cart/Cart.js
@@ -5,7 +5,8 @@ import CartPriceInfo from "../../components/cart/CartPriceInfo";
 import "./Cart.css";
 import {Link, useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
-import { ChakraProvider, Divider } from '@chakra-ui/react';
+import {ChakraProvider, Divider, Text, Button, Stack, Flex, Image} from '@chakra-ui/react';
+import EmptyCart from "../../resources/cart/emptyCart.png";
 
 function Cart(){
     const {userName} = useParams();
@@ -38,27 +39,42 @@ function Cart(){
                 </div>
                 <Divider orientation='horizontal' color='rgb(239, 239, 239)'/>
                 {cart.length === 0 ?
-                    <div className="main">
+                    <div className="main cartHeader">
                         {/* 장바구니없을때 */}
-                        <h2>장바구니</h2>
-                        <Divider orientation='horizontal' color='rgb(239, 239, 239)'/>
-                        <div>
-                            {/* 적절한이미지*/}
-                            장바구니에 담긴 책이 없습니다.
-                        </div>
-                        <Link to="/">
-                            <button>책 담으러 가기</button>
-                        </Link>
+                        <Text as='b' fontSize='4xl'>장바구니</Text>
+                        <Divider orientation='horizontal' color='rgb(239, 239, 239)' mt={5} mb={20}/>
+                        <Flex
+                            height="100vh" /* 화면의 전체 높이를 차지하도록 설정 */
+                            justifyContent="center" /* 가로 중앙 정렬 */
+                        >
+                            <Stack spacing={5} direction="column" ml={20} textAlign="center">
+                                <Stack spacing={1} direction="column" textAlign="center">
+                                    <Text as='b' fontSize='2xl' color='darkslategray'> 장바구니에 담긴 책이 없습니다. </Text>
+                                    <Image src={EmptyCart} transform="scale(0.7)" />
+                                </Stack>
+                                <Link to="/">
+                                    <Button colorScheme="blue" variant="solid" size='lg' width={195}>
+                                        책 담으러 가기
+                                    </Button>
+                                </Link>
+                            </Stack>
+                        </Flex>
                     </div>
                     :
                     <div>
                     {/* 장바구니에상품있을 때 */}
                     <div className="main">
                         <CartHeader userName={userName} cart={cart} setCart={setCart}/>
-                        <div id="Book-Price">
-                            <CartBookList userName={userName} cart={cart} setCart={setCart}/>
-                            <CartPriceInfo cart={cart}/>
-                        </div>
+                        <Flex
+                            height="100vh"
+                            justifyContent="center" /* 가로 중앙 정렬 */
+                            background='rgb(248, 248, 248)'
+                        >
+                            <div id="Book-Price">
+                                <CartBookList userName={userName} cart={cart} setCart={setCart}/>
+                                <CartPriceInfo cart={cart}/>
+                            </div>
+                        </Flex>
                     </div>
                 </div>
                 }
